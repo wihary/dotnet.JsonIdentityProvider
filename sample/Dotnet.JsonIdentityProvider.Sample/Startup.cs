26aff37b0fbcf00decd384a656ff2b754eed8b0b
Whary <william.hary@capgemini.com>
2018-06-13 21:31:16 +0200

    Review Repo structure and added Tests project

--- sample/Dotnet.JsonIdentityProvider.Sample/Startup.cs
+++ sample/Dotnet.JsonIdentityProvider.Sample/Startup.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Dotnet.JsonIdentityProvider.Services;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace Dotnet.JsonIdentityProvider.Sample
+﻿namespace Dotnet.JsonIdentityProvider.Sample
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Dotnet.JsonIdentityProvider.Services;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.HttpsPolicy;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
---
+++ test/Dotnet.JsonIdentityProvider.Tests/IdentityProvider/Model/UserModelTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Dotnet.JsonIdentityProvider.Tests.IdentityProvider.Model
+{
+    public class UserModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
