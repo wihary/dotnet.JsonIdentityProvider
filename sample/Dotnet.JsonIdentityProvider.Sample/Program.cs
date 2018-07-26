fcb30ec2f65303a312e29b2615329f38a2f98d93
WHARY <whary.fr@gmail.com>
2018-07-26 12:10:42 +0200

    [Config] Modification port d'écoute du projet sample

--- sample/Dotnet.JsonIdentityProvider.Sample/Program.cs
+++ sample/Dotnet.JsonIdentityProvider.Sample/Program.cs
@@ -19,6 +19,7 @@ namespace Dotnet.JsonIdentityProvider.Sample
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:5001")
                 .UseStartup<Startup>();
     }
 }
